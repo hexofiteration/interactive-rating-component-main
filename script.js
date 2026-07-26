6671858620615020bbb6d0ce818ec155965497bd
hexofiteration <hexofiteration@gmail.com>
2026-07-26 23:03:27 +0200

    Toggle between states on submit
    Prevent form default submission behavior
    Hide rating-state and reveal thank-you-state on submit click

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ let btnSubmit = ratingState.querySelector("#submit");
 let thankYouState = document.querySelector(".thank-you-state");
 let rating = thankYouState.querySelector("#rating");
 
+rating.textContent = 5;
+
 radioBtns.forEach((element) => {
   element.addEventListener("change", (event) => {
     rating.textContent = event.target.value;
   });
+});
+
+btnSubmit.addEventListener("click", (event) => {
+  event.preventDefault();
+  ratingState.style.display = "none";
+  thankYouState.style.display = "block";
+  console.log("clicked");
 });
