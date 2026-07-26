23c1c7a3a74e0ce3425bedaccddef7395ecaa5ff
hexofiteration <hexofiteration@gmail.com>
2026-07-26 22:39:51 +0200

    update rating display on radio selection

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let radioBtns = ratingState.querySelectorAll('input[name="rating"]');
 let btnSubmit = ratingState.querySelector("#submit");
 let thankYouState = document.querySelector(".thank-you-state");
 let rating = thankYouState.querySelector("#rating");
+
+radioBtns.forEach((element) => {
+  element.addEventListener("change", (event) => {
+    rating.textContent = event.target.value;
+  });
+});
